64be676114a1aac80108eaf1418e450ad7359684
kyushun <27068326+kyushun@users.noreply.github.com>
2019-08-24 21:56:39 +0900

    Header の追加

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template lang="pug">
   #app
-    img(alt="Vue logo" src="./assets/logo.png")
-    HelloWorld(msg="Welcome to Your Vue.js App")
+    Header
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/Header";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    Header
   }
 };
 </script>
@@ -20,8 +19,6 @@ export default {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,77 @@
+<template lang="pug">
+div
+  transition(name="scroll")
+    .header(v-if="show")
+      .content
+        .title
+          | Portfolio
+        .section
+          div Profile
+          div History
+          div Products
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data: function() {
+    return {
+      show: false
+    };
+  },
+  methods: {
+    display: function() {
+      this.show = true;
+    }
+  },
+  mounted: function() {
+    this.display();
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.header {
+  margin: 1.5rem auto 0;
+  padding: 1rem 2rem;
+  width: 75%;
+  border-radius: 16px;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+}
+
+.content {
+  display: flex;
+  justify-content: space-between;
+
+  .title {
+    font-weight: bold;
+  }
+
+  .section {
+    display: flex;
+
+    div {
+      padding: 0 1rem;
+      cursor: pointer;
+    }
+  }
+}
+
+.scroll-enter-active {
+  animation: scroll-in 0.5s;
+}
+.scroll-leave-active {
+  animation: scroll-in 0.5s reverse;
+}
+@keyframes scroll-in {
+  0% {
+    transform: translateY(-60px);
+  }
+  50% {
+    transform: translateY(10px);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+</style>
