edbc0085f59bcece225ca7d5a884a8cc2308ad67
kyushun <27068326+kyushun@users.noreply.github.com>
2019-09-14 19:18:49 +0900

    GA の設定を追加

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 module.exports = {
+  chainWebpack: config => {
+    config.plugin("html").tap(options => {
+      options[0].minify = {
+        removeComments: true,
+        collapseWhitespace: false,
+        removeAttributeQuotes: false,
+        collapseBooleanAttributes: false,
+        removeScriptTypeAttributes: false
+      };
+      return options;
+    });
+  },
   css: {
     loaderOptions: {
       scss: {
