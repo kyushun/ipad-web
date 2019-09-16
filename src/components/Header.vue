78fa61b78c25acc3aef49c9feba31b0822bb5443
kyushun <27068326+kyushun@users.noreply.github.com>
2019-09-16 15:51:29 +0900

    header の背景をブラーにする

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
-transition(name="scroll")
-  .header(v-if="show")
+  .header
+    .bg
     .content
       .title
         | Yushun Kotani's Portfolio
@@ -51,83 +51,83 @@ export default {
   right: 0;
   margin: 0 auto;
   padding: 3.5rem 1rem 0.5rem;
-  background-color: rgba(255, 255, 255, 0.85);
-  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
   z-index: 999;
   white-space: nowrap;
-  overflow-x: auto;
   -webkit-overflow-scrolling: touch;
 
   @include mq(sm) {
     padding: 3.5rem 2rem 0.5rem;
   }
-}
 
-.content {
-  margin: 0 auto;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
+  .bg {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    backdrop-filter: saturate(180%) blur(20px);
+    background-color: rgba(255, 255, 255, 0.7);
+    z-index: -1;
 
-  @include mq(sm) {
-    width: 75%;
+    &::after {
+      position: absolute;
+      content: "";
+      top: 100%;
+      width: 100%;
+      height: 1px;
+      background-color: rgba(29, 29, 31, 0.1);
+    }
   }
 
-  .title {
-    padding: 0.5rem 1rem 0.5rem 0;
-    font-weight: 900;
-  }
+  .content {
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    z-index: 2;
 
-  .last-edited-at {
-    padding: 0.25rem;
-    font-size: 0.5rem;
-    font-weight: 300;
-    border: solid 1px #ccc;
-    border-radius: 6px;
-  }
-
-  .section {
-    display: none;
-    overflow-x: auto;
-    flex: 1;
-    justify-content: flex-end;
-
-    @include mq() {
-      display: flex;
+    @include mq(sm) {
+      width: 75%;
     }
 
-    a {
-      padding: 0.5rem 1rem;
-      cursor: pointer;
-      color: $color-black;
-      text-decoration: none;
-      white-space: nowrap;
-      border-radius: 8px;
-      transition: background-color 0.25s;
+    .title {
+      padding: 0.5rem 1rem 0.5rem 0;
+      font-weight: 900;
+    }
 
-      &:hover {
-        background-color: rgba(0, 0, 0, 0.08);
+    .last-edited-at {
+      padding: 0.25rem;
+      font-size: 0.5rem;
+      font-weight: 300;
+      border: solid 1px #ccc;
+      border-radius: 6px;
+    }
+
+    .section {
+      display: none;
+      overflow-x: auto;
+      flex: 1;
+      justify-content: flex-end;
+
+      @include mq() {
+        display: flex;
+      }
+
+      a {
+        padding: 0.5rem 1rem;
+        cursor: pointer;
+        color: $color-black;
+        text-decoration: none;
+        white-space: nowrap;
+        border-radius: 8px;
+        transition: background-color 0.25s;
+
+        &:hover {
+          background-color: rgba(0, 0, 0, 0.08);
+        }
       }
     }
-  }
-}
-
-.scroll-enter-active {
-  animation: scroll-in 0.5s;
-}
-.scroll-leave-active {
-  animation: scroll-in 0.5s reverse;
-}
-@keyframes scroll-in {
-  0% {
-    transform: translateY(-60px);
-  }
-  50% {
-    transform: translateY(10px);
-  }
-  100% {
-    transform: translateY(0);
   }
 }
 </style>
