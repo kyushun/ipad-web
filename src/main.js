d9daff90acd458aa38172e225812e6d194cb891e
kyushun <27068326+kyushun@users.noreply.github.com>
2019-09-16 22:12:44 +0900

    スクリプトをインラインから消す

--- src/main.js
+++ src/main.js
@@ -2,6 +2,43 @@ import Vue from "vue";
 import App from "./App.vue";
 import vueSmoothScroll from "vue2-smooth-scroll";
 
+if (process.env.NODE_ENV === "production") {
+  window.dataLayer = window.dataLayer || [];
+  const gtag = function() {
+    window.dataLayer.push(arguments);
+  };
+  const accessToken = (() => {
+    const params = new URLSearchParams(window.location.search);
+    const token = params.get("access_token");
+    if (token) {
+      localStorage.accessToken = token;
+      return token;
+    } else {
+      if (localStorage.accessToken) {
+        // URL に access_token パラムを追加
+        params.append("access_token", localStorage.accessToken);
+        window.history.replaceState(
+          {},
+          null,
+          `${window.location.pathname}?${params}`
+        );
+      }
+      return localStorage.accessToken || null;
+    }
+  })();
+  gtag("js", new Date());
+  gtag("config", "UA-147956404-1", { user_id: accessToken });
+
+  // URL フラグメントを削除
+  if (window.location.hash !== "") {
+    history.replaceState(
+      {},
+      null,
+      window.location.href.replace(window.location.hash, "")
+    );
+  }
+}
+
 Vue.config.productionTip = false;
 Vue.use(vueSmoothScroll, { offset: -50, updateHistory: false });
 
