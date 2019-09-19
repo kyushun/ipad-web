7a5647982427bbadedee8977b4fb9dc032d0bdbd
kyushun <27068326+kyushun@users.noreply.github.com>
2019-09-19 10:07:03 +0900

    非推奨ブラウザメッセージを追加

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vueSmoothScroll from "vue2-smooth-scroll";
+import Swal from "sweetalert2";
+import UAParser from "ua-parser-js";
 
 if (process.env.NODE_ENV === "production") {
   window.dataLayer = window.dataLayer || [];
@@ -37,6 +39,32 @@ if (process.env.NODE_ENV === "production") {
       window.location.href.replace(window.location.hash, "")
     );
   }
+}
+
+// 非推奨ブラウザ
+const recommendedBrowsers = ["Blink", "WebKit", "Gecko"];
+const ua = new UAParser();
+if (
+  !localStorage.confirmDeprecatedBrowser &&
+  recommendedBrowsers.indexOf(ua.getEngine().name) == -1
+) {
+  setTimeout(() => {
+    Swal.fire({
+      type: "error",
+      width: 600,
+      title: "お使いのブラウザは対応していません",
+      html:
+        "このサイトはお使いのブラウザでの動作確認を行っておりません。<br />ページの表示が正しく行われない場合があります。",
+      footer:
+        '<a href="https://www.google.com/intl/ja_jp/chrome/" target="_blank">推奨ブラウザ (Google Chrome) をダウンロードする</a>',
+      showCancelButton: false,
+      confirmButtonText: "承知の上で閲覧する"
+    }).then(result => {
+      if (result.value) {
+        localStorage.confirmDeprecatedBrowser = true;
+      }
+    });
+  }, 2000);
 }
 
 Vue.config.productionTip = false;
