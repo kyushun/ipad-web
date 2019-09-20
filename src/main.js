93edc14f5a974c74c66efe28f4249a4e334f4a68
kyushun <27068326+kyushun@users.noreply.github.com>
2019-09-20 09:54:18 +0900

    onload で呼ぶようにする

--- src/main.js
+++ src/main.js
@@ -41,31 +41,33 @@ if (process.env.NODE_ENV === "production") {
   }
 }
 
-// 非推奨ブラウザ
-const recommendedBrowsers = ["Blink", "WebKit", "Gecko"];
-const ua = new UAParser();
-if (
-  !localStorage.confirmDeprecatedBrowser &&
-  recommendedBrowsers.indexOf(ua.getEngine().name) == -1
-) {
-  setTimeout(() => {
-    Swal.fire({
-      type: "error",
-      width: 600,
-      title: "お使いのブラウザは対応していません",
-      html:
-        "このサイトはお使いのブラウザでの動作確認を行っておりません。<br />ページの表示が正しく行われない場合があります。",
-      footer:
-        '<a href="https://www.google.com/intl/ja_jp/chrome/" target="_blank">推奨ブラウザ (Google Chrome) をダウンロードする</a>',
-      showCancelButton: false,
-      confirmButtonText: "同意の上で閲覧する"
-    }).then(result => {
-      if (result.value) {
-        localStorage.confirmDeprecatedBrowser = true;
-      }
-    });
-  }, 2000);
-}
+window.onload = () => {
+  // 非推奨ブラウザ
+  const recommendedBrowsers = ["Blink", "WebKit", "Gecko"];
+  const ua = new UAParser();
+  if (
+    !localStorage.confirmDeprecatedBrowser &&
+    recommendedBrowsers.indexOf(ua.getEngine().name) == -1
+  ) {
+    setTimeout(() => {
+      Swal.fire({
+        type: "error",
+        width: 600,
+        title: "お使いのブラウザは対応していません",
+        html:
+          "このサイトはお使いのブラウザでの動作確認を行っておりません。<br />ページの表示が正しく行われない場合があります。",
+        footer:
+          '<a href="https://www.google.com/intl/ja_jp/chrome/" target="_blank">推奨ブラウザ (Google Chrome) をダウンロードする</a>',
+        showCancelButton: false,
+        confirmButtonText: "同意の上で閲覧する"
+      }).then(result => {
+        if (result.value) {
+          localStorage.confirmDeprecatedBrowser = true;
+        }
+      });
+    }, 1000);
+  }
+};
 
 Vue.config.productionTip = false;
 Vue.use(vueSmoothScroll, { offset: -50, updateHistory: false });
